19aba924e723c3d9abb80086e0ca6aad27d0c0bc
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-11-25 23:05:37 +0530

    getElementsByClassName & Array.from

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const grandparent  = document.getElementById("grandparent-id");
+const parents = document.getElementsByClassName("parent")
 
 function changeColor(element){
     element.style.background = '#333';
 }
 
-changeColor(grandparent)
+//changeColor(grandparent)
+
+Array.from(parents).forEach(changeColor)
