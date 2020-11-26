34cc0392217af18cbe45a74f70131a2cbcb056c7
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-11-26 16:28:43 +0530

    querySelectorAll

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //const grandparent  = document.getElementById("grandparent-id");
-const grandparent = document.querySelector("#grandparent-id")
-const parents = document.getElementsByClassName("parent")
-
+//const grandparent = document.querySelector("#grandparent-id")
+//const parents = document.getElementsByClassName("parent")
+const children = document.querySelectorAll(".child");
 function changeColor(element){
     element.style.background = '#333';
 }
 
-changeColor(grandparent)
-
+//changeColor(grandparent)
+Array.from(children).forEach(changeColor);
 //Array.from(parents).forEach(changeColor)
