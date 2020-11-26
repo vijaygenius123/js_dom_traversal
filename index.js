bde8aaf7d4d0e165c52d7ca5ce12c7a17ac04e9c
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-11-26 23:16:56 +0530

    parentElement

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 //const grandparent  = document.getElementById("grandparent-id");
 //const grandparent = document.querySelector("#grandparent-id")
 //const parents = document.getElementsByClassName("parent")
-const children = document.querySelectorAll(".child");
+//const children = document.querySelectorAll(".child");
+const chileOne = document.querySelector("#child-one");
+const parent = chileOne.parentElement;
 function changeColor(element){
     element.style.background = '#333';
 }
 
 //changeColor(grandparent)
-Array.from(children).forEach(changeColor);
+//Array.from(children).forEach(changeColor);
 //Array.from(parents).forEach(changeColor)
+changeColor(parent)
